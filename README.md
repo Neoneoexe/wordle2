ac9d67c5b702cf734d41f8c7d8e63f1792cd20e5
neoneoexe <albin.rushiti99@gmail.com>
2022-10-30 19:02:26 +0100

    nono

---
+++ README.md
@@ -0,0 +1,31 @@
+# Wordle 2.0
+
+Inspired by [Wordle](https://www.nytimes.com/games/wordle/index.html) I made my own clone.
+
+- NO limits on the words.
+
+- the words that are generated are not the same as the real game.
+
+### Tech
+
+- NextJS
+- React
+- TypeScript
+
+
+### Want a new feature?
+
+- [ Upcoming feature ] 
+- A battle wordle with your friend to see who guesses first, implementation with socket.io
+
+
+### Live Site 
+
+Deployed site at: [Available here]()
+
+
+### BUGS:
+
+- None at the time being
+
+Submit a issue [here]() if you spot any bugs.
---
+++ pages/index.tsx
@@ -0,0 +1,49 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import GameComponent from "../components/game.component";
+import NavbarComponent from "../components/navbar.component";
+import { StorageService } from "../services/storage.service";
+import { WordleState } from "../types/WordleState";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+  const [gameNumber, setGameNumber] = useState<number>(-1);
+  const [gameState, setGameState] = useState<WordleState>();
+
+  useEffect(() => {
+    // Delay until router is ready (JS fully loaded on page)
+    if (!router || !router.isReady) {
+      return;
+    }
+
+    const routerGameNumber = Number.parseInt(router.query.gameNumber as string);
+    const storageGameNumber = StorageService.getLastPlayedGame();
+    const gameNumberInUse = routerGameNumber || storageGameNumber;
+
+    setGameState(StorageService.getGameState(gameNumberInUse));
+    setGameNumber(gameNumberInUse);
+  }, [router]);
+
+  if (gameNumber == -1) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <NavbarComponent
+        title={`Wordle 2.0`}
+        showHistory={true}
+        showReturnToGame={false}
+      />
+
+      <Head>
+        <title>My Wordle - Game {gameNumber}</title>
+      </Head>
+      <GameComponent gameNumber={gameNumber} initialState={gameState} />
+    </>
+  );
+};
+
+export default Home;
